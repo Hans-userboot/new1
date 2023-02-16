da3ae130f2bb9032d251da03b563850ff78bb9de
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2023-02-17 00:02:11 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # (©)Codexbotz
-# Recode by @mrismanaziz
+# Recode by @XYZUZUX
 # t.me/SharingUserbot & t.me/Lunatic0de
 
 import logging
@@ -23,7 +23,7 @@ API_HASH = os.environ.get("API_HASH", "")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
 # NAMA OWNER
-OWNER = os.environ.get("OWNER", "mrismanaziz")
+OWNER = os.environ.get("OWNER", "XYZUZUX")
 
 # Protect Content
 PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
