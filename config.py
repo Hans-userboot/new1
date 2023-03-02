e4d508539956c22f3b0b6dd422a46f8854f8e28c
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2023-03-02 22:17:00 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6120800978:AAEtDCUAdppxHZGftvCkGbzzao5IRMSnhDk")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6118968145:AAGvfCyKbgHJyDfWKEBQsuiApLh485BAsDA")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "11662467"))
@@ -20,7 +20,7 @@ APP_ID = int(os.environ.get("APP_ID", "11662467"))
 API_HASH = os.environ.get("API_HASH", "325e13dfb76973f4bca9bdae2678bbbe")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001536630858"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001868594409"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "XYZUZUX")
@@ -39,8 +39,8 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://ytadjcut:oORvVKpTnRy2pwJAmGOGhizVO5WbGqKy@arjuna.db.elephantsql.com/ytadjcut")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001710357840"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-100152164082"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -50,7 +50,7 @@ START_MSG = os.environ.get(
     "<b>Hello {first}</b>\n\n<b>Saya dapat menyimpan file pribadi di Channel Tertentu dan pengguna lain dapat mengaksesnya dari link khusus.</b>",
 )
 try:
-    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "1640245073 5178772086").split())]
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "5178772086").split())]
 except ValueError:
     raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
 
